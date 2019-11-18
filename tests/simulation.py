bb40e1365f8086e6cf275ff2d0a69be641ee06df
Michael <m.aichmueller@gmail.com>
2019-11-18 17:14:24 +0100

    improved simulation method
    .

--- tests/simulation.py
+++ tests/simulation.py
@@ -9,11 +9,7 @@ from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def population_perc(h):
-    return h ** 2
-
-
-def chance_modifier(level, max_chance):
+def chance_drop_per_level(level, max_chance):
     return max_chance / (10 ** level)
 
 
@@ -22,15 +18,16 @@ def alternating_signs(length):
 
 
 def simulate(
-        nr_genes=500,
-        master_genes=None,
-        levels_of_dependency=5,
-        max_connection_chance=0.9,
+        nr_genes: int = 500,
+        master_genes: Union[int, np.ndarray, List] = None,
+        levels_of_dependency: int = 5,
+        max_connection_chance: float = 0.9,
+        chance_per_level_func=chance_drop_per_level,
 ):
     # =====================
     # PARAMETERS
     # --------------------
-    gene_names = [r"G_" + str(i) + "" for i in range(nr_genes)]
+    gene_names = np.array([r"G_" + str(i) + "" for i in range(nr_genes)])
     if master_genes is None:
         master_genes = gene_names[0:3]
     elif isinstance(master_genes, int):
@@ -49,7 +46,7 @@ def simulate(
     population_per_level = np.diff((np.power(np.linspace(0, 1, nr_dep_levels), 2) * nr_genes)).astype(int)
     population_per_level = np.array([len(master_genes)] + population_per_level.tolist())
     population_per_level[-1] += nr_genes - population_per_level.sum()
-    chance_per_level = {level: chance_modifier(level, max_connection_chance) for level in range(nr_dep_levels)}
+    chance_per_level = {level: chance_per_level_func(level, max_connection_chance) for level in range(nr_dep_levels)}
 
     genes_to_level = dict()
     levels_to_genes: dict = defaultdict(list)
@@ -85,7 +82,7 @@ def simulate(
             parents[this_level] = parent_pool[parent_mask]
             nr_coeffs = len(parents[this_level])
             signs = alternating_signs(nr_coeffs)
-            coeffs += [1.5 * np.random.rand(nr_coeffs) * signs]
+            coeffs += [1. * np.random.rand(nr_coeffs) * signs]
 
         if coeffs:
             coeffs = np.concatenate(coeffs)
@@ -95,7 +92,6 @@ def simulate(
                                  NoiseGenerator("normal",
                                                 loc=0, scale=np.random.rand() * 1)]
 
-
     gene_tex_names = {name: r"$G_{" + str(i) + "}$" for name, i in zip(gene_names, range(nr_genes))}
     cn = SCM(assignment_dict, variable_tex_names=gene_tex_names)
     return cn
@@ -104,22 +100,22 @@ def simulate(
 def analyze_distributions(
         scm_net,
         sample=None,
-        gene=None,
+        genes=None,
         figsize=(20, 20),
         bins=50
 ):
-    if gene is None:
+    if genes is None:
         genes = [g for i, g in enumerate(scm_net.get_variables()) if i < 100]
     if sample is None:
         rs = np.random.RandomState()
         sample = scm_net.sample(10000)
         sample = pd.DataFrame(rs.poisson(np.exp(sample)), columns=sample.columns)
 
-    sample.hist(bins=bins, figsize=figsize)
+    sample[genes].hist(bins=bins, figsize=figsize)
     plt.show()
 
-    def quadr_poly(x, a, b):
-        return a * np.power(x, 2) + b
+    def quadr_poly(mu, phi):
+        return phi * np.power(mu, 2) + mu
 
     mean = sample.mean(axis=0)
     var = sample.var(axis=0)
@@ -135,27 +131,13 @@ def analyze_distributions(
     plt.plot(mean_sorted, quadr_poly(mean_sorted, *popt), color="red")
     plt.title("Mean-Variance-Relationship")
     plt.show()
-    # nr_genes = len(genes)
-    # sqrt = np.sqrt(nr_genes)
-    # is_int_sqrt = sqrt == (nr_genes // sqrt)
-    # if is_int_sqrt:
-    #     sqrt = int(sqrt)
-    #     subplots = (sqrt, sqrt)
-    # else:
-    #     sqrt = int(sqrt)
-    #     subplots = (sqrt, sqrt + 1)
-    #
-    # fig, ax = plt.subplots(subplots, figsize=figsize)
-    # for i, g in enumerate(genes):
-    #
-    #     ax[i % sqrt, i // sqrt].plot()
 
 
 if __name__ == '__main__':
 
-    causal_net = simulate(50, 3)
+    causal_net = simulate(1000, 2)
     print(causal_net)
-    causal_net.plot(alpha=0.5)
+    causal_net.plot(False, node_size=50, alpha=0.5)
     analyze_distributions(scm_net=causal_net)
 
 
