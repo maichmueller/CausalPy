8d4f65162c3133f275cb242573950476a321999e
Michael <m.aichmueller@gmail.com>
2020-01-14 18:23:30 +0100

    wip

--- causalpy/neural_networks/regularization.py
+++ causalpy/neural_networks/regularization.py
@@ -62,10 +62,10 @@ class BinaryConcreteDist:
         logits = np.log(x) - np.log(1 - x)
         return torch.sigmoid(torch.as_tensor(logits * self.beta - self.log_alpha))
 
-    def quantile(self, x: Union[np.ndarray, float]):
-        x = torch.as_tensor(x)
-        logits = torch.log(x) - torch.log(-x + 1)
-        return torch.sigmoid(logits * self.beta - self.log_alpha).clamp(min=0, max=1)
+    def quantile(self, p: Union[np.ndarray, float]):
+        p = torch.as_tensor(p)
+        logits = torch.log(p) - torch.log(-p + 1)
+        return torch.sigmoid((logits + self.log_alpha / self.beta)).clamp(min=0, max=1)
 
 
 class HardConcreteDist:
@@ -85,13 +85,33 @@ class HardConcreteDist:
         # the function h(x) = min(1, max(0, x)) is implemented in torch as Hardtanh(0, 1)(x)
         self.hard_tanh = Hardtanh(0, 1)
 
+    def get_base_dist(self):
+        return self.q_dist
+
     def rsample(self, size: Tuple = (1,)):
         s = self.q_dist.rsample(size=size)
         s_tilde = s * (self.zeta - self.gamma) + self.gamma
         return self.hard_tanh(s_tilde)
 
+    def pdf(self, x: Union[np.ndarray, torch.Tensor, float]):
+        if 0 < x < 1:
+            edge_probs = self.q_dist.quantile(np.ndarray([0, 1]))
+            return (edge_probs[1] - edge_probs[0]) * self.q_dist.pdf(x)
+        elif x == 1:
+            return 1 - self.q_dist.quantile(1)
+        elif x == 0:
+            return self.q_dist.quantile(0)
+        else:
+            return 0
 
-class L0Regularizer(torch.nn.Module):
+    def cdf(self, x: Union[Iterable, float]):
+        raise NotImplementedError
+
+    def quantile(self, p: Union[Iterable, float]):
+        raise NotImplementedError
+
+
+class L0Mask(torch.nn.Module):
     """
     The variable names in this class are chosen to resemble the names given to them in the original paper:
     'LEARNING SPARSE NEURAL NETWORKS THROUGH L0 REGULARIZATION' (https://arxiv.org/pdf/1712.01312.pdf)
@@ -103,6 +123,8 @@ class L0Regularizer(torch.nn.Module):
         self,
         parameters: Iterable,
         dimensions_in: Optional[int] = None,
+        dimensions_out: Optional[int] = None,
+        temperature: float = 2.0 / 3.0,
         gamma: float = -0.1,
         zeta: float = 1.1,
         initial_sparsity_rate: float = 0.5,
@@ -120,32 +142,35 @@ class L0Regularizer(torch.nn.Module):
         else:
             self.device = device
 
-        self.weights = []
-        for params in parameters:
-            init_weights = torch.nn.Parameter(
-                torch.empty(params.shape, device=self.device), requires_grad=True
-            )
-            self.weights.append(init_weights)
+        for i, params in enumerate(parameters):
+            if i > 0:
+                raise ValueError("Currently the parameters of only a single layer is supported.")
+            dim_in, dim_out = params.shape
 
-        self.dim_in = (
-            self.weights[0].size(1) if dimensions_in is None else dimensions_in
-        )
-        self.weights = torch.nn.ParameterList(self.weights)
+        self.dim_in = dim_in if dimensions_in is None else dimensions_in
+        self.dim_out = dim_out if dimensions_out is None else dimensions_out
+
         # the log alpha locations are learnable parameters
         self.log_alpha = torch.nn.Parameter(
             torch.empty(self.dim_in, dtype=torch.float32), requires_grad=True
         )
-        self.reinitialize_weights()
+        self.initialize_weights()
 
-        self.hard_concrete_dist = HardConcreteDist()
+        self.hard_concrete_dist = HardConcreteDist(
+            q_dist=BinaryConcreteDist(
+                alpha=self.log_alpha.exp(), beta=temperature, seed=seed
+            ),
+            gamma=gamma,
+            zeta=zeta,
+            seed=seed,
+        )
 
+        # the function named g in the paper, which computes z
         self.hard_tanh = Hardtanh(min_val=0, max_val=1)
 
         self.random_state = np.random.default_rng(seed)
 
-        self.transformation_f = transformation_f  # the deterministic and differentiable transformation f
-
-    def reinitialize_weights(self):
+    def initialize_weights(self):
         torch.nn.init.kaiming_normal(self.weights, mode="fan_out")
 
         self.log_alpha.data.normal_(
@@ -154,58 +179,64 @@ class L0Regularizer(torch.nn.Module):
             std=1e-2,
         )
 
-    def sample_epsilon(self, size=(1,), low=EPS, high=1 - EPS):
+    def forward(self, batch_size: int):
+        # if self.local_rep or not self.training:
+        z = self.sample_z(batch_size, sample=self.training)
+        # else:
+        #     weights = self.sample_weights()
+        #     tensor_out = tensor_in.mm(weights)
+        return z
+
+    def _sample_epsilon(self, size=(1,), low=EPS, high=1 - EPS):
         """
         distribution of the noise 'epsilon'
         """
         return torch.as_tensor(self.random_state.uniform(low=low, high=high, size=size))
 
-    def sample_z(self, batch_size: int, sample=True):
-        """
+    def _cdf_stretched_concrete(self, x, *args, **kwargs):
+        return self.hard_concrete_dist.get_base_dist().cdf(
+            (x - self.gamma) / (self.zeta - self.gamma), *args, **kwargs
+        )
+
+    def _quantile_stretched_concrete(self, p, *args, **kwargs):
+        return (
+            self.hard_concrete_dist.get_base_dist().quantile(p, *args, **kwargs)
+            * (self.zeta - self.gamma)
+            + self.gamma
+        )
+
+    def sample_mask(self, batch_size: int, sample=True):
+        r"""
         Sample the hard-concrete gates for training and use a deterministic value for testing
+
+        Notes
+        -----
+        Quote paper (p.5):
+
+        "at test time the following estimator is used for the final parameters:
+
+        .. math:: \hat{z} = \min (1, \max(0, Sigmoid(\log \alpha)(\zeta - \gamma) + \gamma),
+        .. math:: \theta^{\star} = \tilde{\theta}^{\star} \cdot \hat{z}
+
         """
         if sample:
-            eps = self.get_eps(self.floatTensor(batch_size, self.in_features))
-            z = self.quantile_concrete(eps)
-            return self.hard_tanh(z)
-        else:  # mode
+            z = self._quantile_stretched_concrete(self._sample_epsilon(self.dim_in))
+            mask = self.hard_tanh(z)
+        else:
+            # test time:
             pi = (
-                self.transformation_f(self.qz_loga)
+                torch.sigmoid(self.qz_loga)
                 .view(1, self.in_features)
                 .expand(batch_size, self.in_features)
             )
-            return self.hard_tanh(pi * (self.zeta - self.gamma) + self.gamma)
+            mask = self.hard_tanh(pi * (self.zeta - self.gamma) + self.gamma)
+        return mask
 
-    def sample_weights(self):
-        z = self.quantile_concrete(self.get_eps(self.floatTensor(self.in_features)))
-        mask = self.hard_tanh(z)
-        return mask.view(self.in_features, 1) * self.weights
-
-    def forward(self, input):
-        if self.local_rep or not self.training:
-            z = self.sample_z(input.size(0), sample=self.training)
-            xin = input.mul(z)
-            output = xin.mm(self.weights)
-        else:
-            weights = self.sample_weights()
-            output = input.mm(weights)
-        if self.use_bias:
-            output.add_(self.bias)
-        return output
-
-    def _reg_w(self):
+    def compute_loss(self, loss_func, tensor_input):
         """Expected L0 norm under the stochastic gates, takes into account and re-weights also a potential L2 penalty"""
         logpw_col = torch.sum(
             -(0.5 * self.prior_prec * self.weights.pow(2)) - self.lamba, 1
         )
-        logpw = torch.sum((1 - self.cdf_qz(0)) * logpw_col)
-        logpb = (
-            0
-            if not self.use_bias
-            else -torch.sum(0.5 * self.prior_prec * self.bias.pow(2))
-        )
-        return logpw + logpb
+        logpw = torch.sum((1 - self._cdf_stretched_concrete(0)) * logpw_col)
 
-    @staticmethod
-    def g(z: torch.Tensor):
-        return torch.min(torch.ones_like(z), torch.max(torch.zeros_like(z), z))
+        return logpw
