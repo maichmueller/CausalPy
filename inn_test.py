7de475fa61962b6346171028bc21d0519cba125f
Michael <m.aichmueller@gmail.com>
2020-01-28 17:35:36 +0100

    Single dim Inn now refactored and working again

--- inn_test.py
+++ inn_test.py
@@ -29,7 +29,7 @@ data = (data-t.mean(data)).to(dev)
 con = con.to(dev)
 
 xV = t.arange(-3, 3, 0.1).unsqueeze(1).to(dev)
-net = singleDimINN.INN(nr_blocks=4, dim=1, nr_layers=30, dim_condition=0).to(dev)
+net = singleDimINN.INN(nr_blocks=4, dim=1, nr_layers=30, dim_condition=n_conDim).to(dev)
 
 win1 = viz.histogram(X=data.squeeze(), opts=dict(numbins=50, title='ground truth'))
 z = net(x=xV, condition=t.zeros(len(xV), n_conDim).to(dev), rev=False)
@@ -38,15 +38,15 @@ win3 = viz.line(X=s(xV), Y=s(std(z) * t.exp(net.log_jacobian_cache)), opts=dict(
 optimizer = t.optim.Adam(net.parameters(), lr=0.01, weight_decay=1e-5)
 
 for i in range(1000):
-    z = net.forward(x=data, condition=con, rev=False)
+    z = net(x=data, condition=con, rev=False)
     grad = net.log_jacobian_cache
-    loss = t.mean(z ** 2 / 2 - grad)
+    loss = (z ** 2 / 2 - grad).mean()
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
     if i % 10 == 0:
         print(loss)
-        z = net(x=xV, y=t.zeros(len(xV), n_conDim).to(dev), rev=False)
+        z = net(x=xV, condition=t.zeros(len(xV), n_conDim).to(dev), rev=False)
 
         viz.line(X=s(xV), Y=s(z), win=win2, opts=dict(title='flow'))
         viz.line(X=s(xV), Y=s(std(z) * t.exp(net.log_jacobian_cache)), win=win3, opts=dict(title='estimated density'))
