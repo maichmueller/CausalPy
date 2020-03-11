9560709270d4af1c7512652d6de65b688d97fb2b
Michael <m.aichmueller@gmail.com>
2020-03-11 13:56:50 +0100

    small scm push before refactor

--- causalpy/bayesian_graphs/scm/assignments/composition.py
+++ causalpy/bayesian_graphs/scm/assignments/composition.py
@@ -11,10 +11,17 @@ class CompositionAssignment(Assignment):
         f(X_S, N) = offset + \sum_{i \in S} a_i * X_i + noise_coeff * N
     """
 
-    def __init__(self, *assignment_funcs: Assignment):
+    def __init__(self, pass_down_noise: bool, *assignment_funcs: Assignment):
         super().__init__()
-        self.assignment_root_node = CompositionNode(assignment_funcs)
+        # All the nodes need to pass down the noise in either case, but the root node
+        # decides whether the passed down noise is the real noise (pass_down_noise == True)
+        # or just dummy 0 noise.
+        # TODO: this brings up the question whether the noise wouldn't need to be handled differently
+        #  overall, as 0 noise could also change the data evaluation as not always f(0) = 0.
+        self.assignment_root_node = CompositionNode(True, assignment_funcs)
+        self.assignment_root_node.pass_down_noise = pass_down_noise
         self.nr_assignments = len(assignment_funcs)
+        self.noise_at_end = pass_down_noise
 
     def __call__(self, noise: Union[float, np.ndarray], *args, **kwargs):
         args = self.parse_call_input(*args, **kwargs)
@@ -36,17 +43,21 @@ class CompositionAssignment(Assignment):
 
 
 class CompositionNode:
-    def __init__(self, assignment_funcs: Tuple[Assignment]):
+    def __init__(self, pass_down_noise: bool, assignment_funcs: Tuple[Assignment]):
+        self.pass_down_noise = pass_down_noise
         self.assignment = assignment_funcs[0]
         remaining_assignments = assignment_funcs[1:]
         if len(remaining_assignments) > 0:
-            self.next_node = CompositionNode(remaining_assignments)
+            self.next_node = CompositionNode(pass_down_noise, remaining_assignments)
         else:
             self.next_node = None
 
     def __call__(self, noise, *args):
         if self.next_node is not None:
-            return self.assignment(self.next_node(noise, *args))
+            if self.pass_down_noise:
+                return self.assignment(self.next_node(noise, *args))
+            else:
+                return self.assignment(noise, self.next_node(0, *args))
         else:
             return self.assignment(noise, *args)
 
@@ -55,6 +66,9 @@ class CompositionNode:
             return 1 + len(self.next_node)
         else:
             return 1
+
+    def set_noise_passing(self, pass_down_noise: bool):
+        self.pass_down_noise = pass_down_noise
 
     def function_str(self, variable_names: Optional[Collection[str]] = None):
         if self.next_node is not None:
